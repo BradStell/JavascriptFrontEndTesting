9641fec40bf2fd471c9c61b3694d3dc84690f783
Brad Stell <brad.stell@kingsmensoftware.com>
2016-03-08 17:09:17 -0500

    First commit

---
+++ Calculator.js
@@ -0,0 +1,25 @@
+﻿var Calculator = {
+
+    Calculate: function (selectedValue, firstNumber, secondNumber) {
+        var result = "Invalid Result";
+
+        switch (selectedValue) {
+            case Operation.Add:
+                result = firstNumber + secondNumber;
+                break;
+            case Operation.Subtract:
+                result = firstNumber - secondNumber;
+                break;
+            case Operation.Multiply:
+                result = firstNumber * secondNumber;
+                break;
+            case Operation.Divide:
+                result = firstNumber / secondNumber;
+                break;
+        }
+
+        return result;
+    }
+};
+
+var Operation = { "Add": 1, "Subtract": 2, "Multiply": 3, "Divide": 4 }
---
+++ CalculatorTest.js
@@ -0,0 +1,24 @@
+﻿/// <reference path="Scripts/jasmine/jasmine.js"/>
+/// <reference path="Calculator.js"/>
+
+describe("calculator functionality", function () {
+    it("Add two numbers", function () {
+        var result = Calculator.Calculate(Operation.Add, 2, 3);
+        expect(result).toBe(2 + 3);
+    });
+
+    it("Subtract two numbers", function () {
+        var result = Calculator.Calculate(Operation.Subtract, 2, 3);
+        expect(result).toBe(2 - 3);
+    });
+
+    it("Multiply two numbers", function () {
+        var result = Calculator.Calculate(Operation.Multiply, 2, 3);
+        expect(result).toBe(2 * 3);
+    });
+
+    it("Divide two numbers", function () {
+        var result = Calculator.Calculate(Operation.Divide, 2, 3);
+        expect(result).toBe(2 / 3);
+    });
+});
